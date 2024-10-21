9da1941fe02f4a3f3b552e8477d3320709edac8d
Tobi Samuel <ufomatobi@gmail.com>
2024-10-21 05:06:43 +0100

    Made changes to the partners criteria

--- src/api/webhook/paystack/route.ts
+++ src/api/webhook/paystack/route.ts
@@ -1,6 +1,6 @@
-import stripe from "stripe";
+// import stripe from "stripe";
 import { NextResponse } from "next/server";
-import { createOrder } from "@/lib/actions/order.actions";
+// import { createOrder } from "@/lib/actions/order.actions";
 
 export async function POST(request: Request) {
   const body = await request.text();
@@ -10,30 +10,30 @@ export async function POST(request: Request) {
 
   let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
-  } catch (err) {
-    return NextResponse.json({ message: "Webhook error", error: err });
-  }
+  // try {
+  //   event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+  // } catch (err) {
+  //   return NextResponse.json({ message: "Webhook error", error: err });
+  // }
 
   // Get the ID and type
-  const eventType = event.type;
+  // const eventType = event.type;
 
   // CREATE
-  if (eventType === "checkout.session.completed") {
-    const { id, amount_total, metadata } = event.data.object;
+  // if (eventType === "checkout.session.completed") {
+  //   const { id, amount_total, metadata } = event.data.object;
 
-    const order = {
-      stripeId: id,
-      eventId: metadata?.eventId || "",
-      buyerId: metadata?.buyerId || "",
-      totalAmount: amount_total ? (amount_total / 100).toString() : "0",
-      createdAt: new Date(),
-    };
+  //   const order = {
+  //     stripeId: id,
+  //     eventId: metadata?.eventId || "",
+  //     buyerId: metadata?.buyerId || "",
+  //     totalAmount: amount_total ? (amount_total / 100).toString() : "0",
+  //     createdAt: new Date(),
+  //   };
 
-    const newOrder = await createOrder(order);
-    return NextResponse.json({ message: "OK", order: newOrder });
-  }
+  //   // const newOrder = await createOrder(order);
+  //   // return NextResponse.json({ message: "OK", order: newOrder });
+  // }
 
   return new Response("", { status: 200 });
 }
